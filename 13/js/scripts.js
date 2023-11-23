4a8c59c37b68d1422430c6a319302a141a416462
RODOLFO GONCALVES DA SILVA <rodolfo.gsilva2@senacsp.edu.br>
2023-11-23 19:54:42 -0300

    Continuando eventos - 23-11

--- 13/js/scripts.js
+++ 13/js/scripts.js
@@ -18,3 +18,59 @@ thirdBtn.addEventListener("click", () => {
     console.log("Evento Removido")
     secondBtn.removeEventListener("click", imprimirMensagem);
 })
+
+const title = document.querySelector("#my-title"); 
+
+// title.addEventListener("click", (event) => {
+//     console.log(event); 
+// });
+
+title.addEventListener("click", (e) => {
+    console.log(e.target); 
+    console.log(e.offsetX); 
+    console.log(e.pointerType); 
+});
+
+const containerBtn = document.querySelector("#btn-container");
+
+const btnInsideContainer = document.querySelector("#div-btn");
+
+containerBtn.addEventListener("click", () => {
+    console.log("Evento 1 - Div");
+})
+
+btnInsideContainer.addEventListener("click", (e) => {
+    e.stopPropagation(); 
+    console.log("Evento 2 - Button");
+});
+
+const link1 = document.querySelector("a") 
+
+link1.addEventListener("click", (e) => {
+    e.preventDefault();
+    console.log("Não alterou nada!");
+});
+
+document.addEventListener("keyup", (e) => {
+    console.log(`Soltou a tecla ${e.key}`); 
+})
+document.addEventListener("keydown", (e) => {
+    console.log(`Pressionou a tecla ${e.key}`); 
+})
+
+const mouseEvents = document.querySelector("#mouse");
+
+mouseEvents.addEventListener("mousedown", () =>{
+    console.log("pressionou o botão");
+});
+mouseEvents.addEventListener("mouseup", () =>{
+    console.log("soltou o botão");
+});
+mouseEvents.addEventListener("dblclick", () =>{
+    console.log("clique duplo");
+});
+
+
+document.addEventListener("mousemove", (e) =>{
+    
+})
