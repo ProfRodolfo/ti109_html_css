a4f7324f8630f3a14ea19d09b0cda93bdba529fa
RODOLFO GONCALVES DA SILVA <rodolfo.gsilva2@senacsp.edu.br>
2023-11-23 20:08:04 -0300

    finalizando eventos - 23-11

--- 13/js/scripts.js
+++ 13/js/scripts.js
@@ -53,10 +53,10 @@ link1.addEventListener("click", (e) => {
 
 document.addEventListener("keyup", (e) => {
     console.log(`Soltou a tecla ${e.key}`); 
-})
+});
 document.addEventListener("keydown", (e) => {
     console.log(`Pressionou a tecla ${e.key}`); 
-})
+});
 
 const mouseEvents = document.querySelector("#mouse");
 
@@ -71,6 +71,22 @@ mouseEvents.addEventListener("dblclick", () =>{
 });
 
 
-document.addEventListener("mousemove", (e) =>{
-    
+// document.addEventListener("mousemove", (e) =>{
+//   console.log(`No eixo X: ${e.x}`);  
+//   console.log(`No eixo y: ${e.y}`);  
+// });
+
+window.addEventListener("scroll", (e) => {
+    if(window.scrollY > 200) {
+        console.log("Passamos de 200px!")
+    }
+});
+
+const input = document.querySelector("#my-input");
+
+input.addEventListener("focus" , (e) => {
+    console.log("Entrou no input");
 })
+input.addEventListener("blur" , (e) => {
+    alert("Saiu do  input");
+}) 
