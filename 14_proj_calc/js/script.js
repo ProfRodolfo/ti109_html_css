5db1dff8f1002d11c605b753e3e4a990ac7a76af
RODOLFO GONCALVES DA SILVA <rodolfo.gsilva2@senacsp.edu.br>
2023-11-28 20:50:54 -0300

    proj calc final sem espaços

--- 14_proj_calc/js/script.js
+++ 14_proj_calc/js/script.js
@@ -3,22 +3,17 @@ const numberInput = document.querySelector("#number");
 const multiplicationInput = document.querySelector("#multiplicator");
 const multiplicationTitle = document.querySelector("#multiplication-title span");
 const multiplicationTable = document.querySelector("#multiplication-operations");
-
 //funções
 // 1 - Crie a tabela
-
 const createTable = (number, multiplicatorNumber) =>{
     multiplicationTable.innerHTML = ""; 
-
     // 2 - Cria a tabuada
     for(i = 1; i <= multiplicatorNumber; i++) {
         const result = number * i;
-
         const template = `<div class="row">
              <div class="operation">${number} x ${i} = </div>
              <div class="result">${result}</div>
         </div>`;
-
         const parser = new DOMParser();
         const htmlTemplate = parser.parseFromString(template, "text/html"); 
         const row = htmlTemplate.querySelector(".row"); 
@@ -26,9 +21,7 @@ const createTable = (number, multiplicatorNumber) =>{
     }
     multiplicationTitle.innerText = number; 
 }; 
-
 // Eventos
-
 multiplicationForm.addEventListener("submit", (e) =>{
     e.preventDefault();
     const multiplicationNumber = numberInput.value;
